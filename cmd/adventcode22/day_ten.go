3a9313dd91ed1be8cf94137863c4500779d9c9ec
Fredrik Andersson <fredrik@piktiv.se>
2022-12-10 10:51:52 +0100

    add solution for day 10 part 1

--- cmd/adventcode22/day_ten.go
+++ cmd/adventcode22/day_ten.go
@@ -1,6 +1,45 @@
 package main
 
-func DayTen() (interface{}, interface{}) {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-	return 0, 0
+func DayTen() (interface{}, interface{}) {
+	openFile := readFileByLines("inputs/d10.txt")
+	if openFile == nil {
+		fmt.Println("could not open puzzle input!")
+		return 0, 0
+	}
+
+	cpuCommands := []string{}
+	for openFile.Scanner.Scan() {
+		input := openFile.Scanner.Text()
+
+		if strings.Contains(input, "addx") {
+			cpuCommands = append(cpuCommands, "noop", input)
+			continue
+		}
+
+		cpuCommands = append(cpuCommands, input)
+	}
+
+	register := 1
+	signal := 0
+	for index, command := range cpuCommands {
+		cycle := index + 1
+		if cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220 {
+			signal += cycle * register
+		}
+		if strings.Contains(command, "addx") {
+			parts := strings.Split(command, " ")
+			addition, _ := strconv.Atoi(parts[1])
+
+			register += addition
+		}
+	}
+
+	openFile.File.Close()
+	return signal, 0
 }
