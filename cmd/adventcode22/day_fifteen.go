c5ae744d4e2a6728b6b4d2ba57f5ef37085f85b5
Fredrik Andersson <fredrik.ks.andersson@gmail.com>
2022-12-17 17:17:42 +0100

    add scaffolding for day fifteen

---
+++ cmd/adventcode22/day_fifteen.go
@@ -0,0 +1,5 @@
+package main
+
+func DayFifteen() (interface{}, interface{}) {
+	return 0, 0
+}
--- cmd/adventcode22/main.go
+++ cmd/adventcode22/main.go
@@ -18,6 +18,7 @@ func main() {
 		DayNine,
 		DayTen,
 		DayEleven,
+		DayFifteen,
 		DaySixteen,
 	}
 
